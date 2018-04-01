b187193e8b395a6519f87175634ae406826343e0
kiababashahi <kiababashahi@gmail.com>
2018-04-01 17:00:57 -0400

    First creation

---
+++ answers/data_frame.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+import sys
+import csv
+
+from pyspark.sql.functions import monotonically_increasing_id
+from pyspark.sql.types import Row
+from pyspark import SparkContext, SparkConf
+file=sys.argv[1]
+n=sys.argv[2]
+conf = SparkConf().setAppName('Kia_bigdata_lab').setMaster('local')
+sc = SparkContext(conf=conf)
+spark=SparkSession.builder.appName("lab3").getOrCreate()
+rd=sc.textFile(file).map(lambda x: Row((x.split(','))[0],x.split(',')[1:]))
+df=rd.toDF(["items","plant"]).withColumn("id",monotonically_increasing_id())
+df=df[["id","items","plant"]].show(int(n))
